094a6f35f47ba383f29eb7e887f0f8c75058dbaf
SumitRana <rana.sumit93@gmail.com>
2018-01-19 12:03:45 +0530

    code made configuration file based

--- scaleSFS/CustomStorage.py
+++ scaleSFS/CustomStorage.py
@@ -1,12 +1,12 @@
 from django.core.files.storage import FileSystemStorage
 import os
 from django.conf import settings
-from .Connector import CustomStorage
+from .Connector import RemoteConnection
 
 # configuration
 # server tuple = ("ip","port","base path","user",current or not boolean,"identity_key path","password")
-StaticServers = (('http://10.5.50.1:80','/var/www/','ubuntu',False,None,"password"),
-				 ('http://10.5.50.1:80','/var/www/','ubuntu',False,"/path/to/pem/sam.pem",None))
+# StaticServers = (('http','10.5.50.1','80','/var/www/','ubuntu',False,None,"password"),
+# 				 ('http://10.5.50.1:80','/var/www/','ubuntu',False,"/path/to/pem/sam.pem",None))
 # Author - Sumit | Sam
 
 class CustomStorage(FileSystemStorage):
@@ -15,7 +15,7 @@ class CustomStorage(FileSystemStorage):
 	currentFileServer = None
 
 	def __init__(self):
-		for server in StaticServers:
+		for server in settings.StaticServers:
 			if server[3]== True:
 				self.currentFileServer = server
 
@@ -23,8 +23,8 @@ class CustomStorage(FileSystemStorage):
 		if self.exists(name):
 			self.delete(name)
 
-		# name = "@"+current_ip+":"+current_ip_port+"@"+"/"+str(name)
-		name = str(self.currentFileServer[0])+str(name)
+		upload_to = name
+		name = str(self.currentFileServer[0])+"://"+str(self.currentFileServer[1])+":"+str(self.currentFileServer[2])+str(name)
 
 		parts = str(name).split("/")
 		filename = parts[len(parts)-1]
@@ -33,13 +33,13 @@ class CustomStorage(FileSystemStorage):
 		# Making file buffer
 		with open(settings.BASE_DIR+"/"+filename,"wb+") as f:
 			for chunk in content.chunks():
-				print("writing chunk")
 				f.write(chunk)
 
 		# uploading file to current remote server
-		self.rc = RemoteConnection(ip_address=self.currentFileServer[0],username=self.currentFileServer[2],
-									identity_key=self.currentFileServer[4],password=self.currentFileServer[5])
-		rt = self.rc.put_file(settings.BASE_DIR+"/"+filename,"/home/ubuntu/",name)
+		self.rc = RemoteConnection(ip_address=self.currentFileServer[1],username=self.currentFileServer[4],
+									identity_key=self.currentFileServer[6],password=self.currentFileServer[7])
+		rt = self.rc.put_file(settings.BASE_DIR+"/"+filename,self.currentFileServer[4],upload_to)
+
 		# deleting file buffer
 		if rt is True:
 			os.remove(settings.BASE_DIR+"/"+filename)
@@ -59,8 +59,6 @@ class CustomStorage(FileSystemStorage):
 		pass
 
 	def url(self,name):
-		# return str("/media/"+name)
-		# return str(protocol+"://"+str(current_ip)+":"+str(current_ip_port)+"/"+name)
 		return name
 
 	def get_availaible_name(self,name):
