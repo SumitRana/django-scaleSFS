aec7ca7a16b618583644842e1bc56eb6a3553017
SumitRana <rana.sumit93@gmail.com>
2018-01-31 18:05:31 +0530

    Change in Custom Storage

--- scaleSFS/CustomStorage.py
+++ scaleSFS/CustomStorage.py
@@ -1,13 +1,13 @@
+# Author - Sumit | Sam
+
 from django.core.files.storage import FileSystemStorage
 import os
 from django.conf import settings
 from .Connector import RemoteConnection
 
 # configuration
-# server tuple = ("ip","port","base path","user",current or not boolean,"identity_key path","password")
-# StaticServers = (('http','10.5.50.1','80','/var/www/','ubuntu',False,None,"password"),
-# 				 ('http://10.5.50.1:80','/var/www/','ubuntu',False,"/path/to/pem/sam.pem",None))
-# Author - Sumit | Sam
+# server tuple = ("protocol","ip","port","base path","user",current or not boolean,"identity_key path","password")
+# StaticServers = (('http','10.5.50.1','80','/var/www/','ubuntu',False,None,"password"), ..)
 
 class CustomStorage(FileSystemStorage):
 
@@ -30,7 +30,7 @@ class CustomStorage(FileSystemStorage):
 		filename = parts[len(parts)-1]
 		folders = parts.remove(filename)
 
-		# Making file bu	ffer
+		# Making file buffer
 		with open(settings.BASE_DIR+"/"+filename,"wb+") as f:
 			for chunk in content.chunks():
 				f.write(chunk)
@@ -38,7 +38,7 @@ class CustomStorage(FileSystemStorage):
 		# uploading file to current remote server
 		self.rc = RemoteConnection(ip_address=self.currentFileServer[1],username=self.currentFileServer[4],
 									identity_key=self.currentFileServer[6],password=self.currentFileServer[7])
-		rt = self.rc.put_file(settings.BASE_DIR+"/"+filename,self.currentFileServer[4],upload_to)
+		rt = self.rc.put_file(settings.BASE_DIR+"/"+filename,self.currentFileServer[3],upload_to)
 
 		# deleting file buffer
 		if rt is True:
